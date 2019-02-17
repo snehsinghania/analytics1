6cff3f2aa541c425314c55614b58b3d23efb4aba
snehsinghania <snehsinghania18@gmail.com>
2019-02-17 11:57:21 +0530

    day2

---
+++ general instruction file 2.R
@@ -0,0 +1,6 @@
+My first r file
+women
+#control + enter to execute command                                                                                 
+?women
+?mean
+#?before the comment to seek help
